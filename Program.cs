07989f8af78e317b391065b3034ed43070715ef4
MinotaurosD <doraemon1970@163.com>
2020-05-27 22:13:18 +0800

    binaray search

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AlgorithmsOfCSharp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] array1 = { 1, 3, 5, 7, 9 };
+            Console.WriteLine(BinarySearch.IndexOf(array1, 5));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/BinarySearch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+/// <summary>
+/// 二分查找
+/// </summary>
+public class BinarySearch
+{
+    /// <summary>
+    /// 二分查找
+    /// </summary>
+    /// <param name="array">升序数组</param>
+    /// <param name="value">目标值</param>
+    /// <returns></returns>
+    public static int IndexOf(int[] array, int value)
+    {
+        //低位索引
+        int low = 0;
+        //高位索引
+        int high = array.Length - 1;
+        //当低位索引小于等于高位索引时，说明循环还未结束，继续循环；否则循环结束，未能查找到目标值
+        //当数组本身长度为0时，不进循环直接返回-1
+        //当数组本身长度为1时，首次进入循环middle为0，如果array[0]不等于value，则第二次循环条件不成立跳出循环
+        while (low <= high)
+        {
+            //获取中位索引
+            int middle = low + (high - low) / 2;
+            //如果目标值小于中位索引的值，那么目标值一定在左侧数组中，于是将高位索引修改为当前的中位索引-1
+            if (value < array[middle])
+            {
+                high = middle - 1;
+            }
+            //如果目标值高于中位索引的值，那么目标值一定在右侧数组中，于是将低位索引修改为当前中位索引+1
+            else if (value > array[middle])
+            {
+                low = middle + 1;
+            }
+            //找到目标值直接返回索引
+            else
+            {
+                return middle;
+            }
+        }
+        return -1;
+    }
+}
