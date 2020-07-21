1dce111f123377a807b1e0ab94f865d71699650a
MinotaurosD <doraemon1970@163.com>
2020-07-21 16:43:32 +0800

    Update QuickSort.cs

--- src/QuickSort.cs
+++ src/QuickSort.cs
@@ -22,13 +22,14 @@ public class QuickSort
     /// <param name="high">结束索引</param>
     private static void Sort(int[] array, int low, int high)
     {
-        if (high <= low)
+        //如果起始大于等于high，说明待排序元素为1，结束循环
+        if (low >= high)
         {
             return;
         }
-        int result = Deparation(array, low, high);
-        Sort(array, low, result - 1);
-        Sort(array, result + 1, high);
+        int index = Partition(array, low, high);
+        Sort(array, low, index - 1);
+        Sort(array, index + 1, high);
     }
 
     /// <summary>
@@ -38,38 +39,48 @@ public class QuickSort
     /// <param name="low">起始索引</param>
     /// <param name="high">结束索引</param>
     /// <returns></returns>
-    private static int Deparation(int[] array, int low, int high)
+    private static int Partition(int[] array, int low, int high)
     {
+        //选定最左侧元素为判断的基准元素
         int value = array[low];
-        int slow = low + 1;
-        int quick = high;
-        while (slow <= quick)
+        //左侧从基准元素右边元素开始
+        int left = low + 1;
+        //右侧从最右侧元素开始
+        int right = high;
+        //左索引小于等于右索引时循环
+        while (left <= right)
         {
-            while (array[slow] < value)
+            //找到左侧第一个大于等于value的索引
+            while (array[left] < value)
             {
-                slow++;
-                if (slow >= high)
+                left++;
+                if (left >= high)
                 {
                     break;
                 }
             }
-            while (array[quick] > value)
+            //找到右侧第一个小于等于value的索引
+            while (array[right] > value)
             {
-                quick--;
-                if (quick <= low)
+                right--;
+                if (right <= low)
                 {
                     break;
                 }
             }
-            if (slow >= quick)
+            //如果左索引大于等于右索引，说明循环结束
+            if (left >= right)
             {
                 break;
             }
-            Swap(array, slow, quick);
+            //交换找到的左侧和右侧的值
+            Swap(array, left, right);
         }
 
-        Swap(array, low, quick);
-        return quick;
+        //此时right指向的元素是左数组最后一个小于等于value的索引，使其与最左侧元素交换，以保证right左侧都比value小，右侧都比value大
+        Swap(array, low, right);
+        //right正好为value所在的索引位置，即此时right位置的值是排序过的
+        return right;
     }
 
     /// <summary>
